0b0136caa7ef49bfb810eb4d640bd31d81617731
Evelyn <evelynbrandao15@gmail.com>
2021-04-13 18:37:17 -0300

    Função de aleatoriedade e Criação de Cards concluídas

--- vanilla/js/main.js
+++ vanilla/js/main.js
@@ -6,34 +6,96 @@ let cont = 0
 
 let amountCard = document.getElementById("numberCards")
 let btnStart = document.getElementById("btnStart")
-
+let game = document.getElementById("game")
 
 
 btnStart.addEventListener("click", function(){
+	btnStart.hidden = true ;
 	console.log("Que o jogo começe");
 	console.log("Qtd.: ", amountCard.value);
 	// Buscar as imagens
+	let obj = {
+		data: amountCard.value,
+		success: function(urls){
+
+			urls = urls.map((v, i) => {return {id: i, img: v}; })
+
+			let cardsOfGame = shuffle ([...urls, ...urls]);
+
+			for(let i=0; i < cardsOfGame.length; i++){
+				createCard(i, cardsOfGame[i].id, cardsOfGame[i].img)
+			}
+
+		},
+		error: function(e){
+			console.log(e)
+		}
+	}
+	ajax(obj);
 	// http://shibe.online/api/shibes?count=1
 	// Gerar Cards
+   
 	// Adicionar Cards na div.game
 })
 
-console.clear()
+// Gerar Cards
+function createCard (id, name ,urlImg) {
+	const html = `
+		${game.innerHTML}
+   		 <div class="card" name="nameCard-${name}" onclick="clickCard(this)" id="${id}">
+   			<div class="back">?</div>
+   			<div class="front"><img src="${urlImg}"></div>
+   		</div>`
+
+	game.innerHTML = html;
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    // Index randomico de 0 a tamanho da array
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // Uso copo para trocar as posições
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+function ajax(param){
+	urls = [
+		"https://cdn.shibe.online/shibes/04ad3e052255ca4f5f2b5d97c27b24ebd1b9f75c.jpg",
+		"https://cdn.shibe.online/shibes/c82ab7c7b9f2b7dde90f015a3faacfe39961607c.jpg",
+		"https://cdn.shibe.online/shibes/bc1c0accd8288f6cb6dc8746213f9ab63da38461.jpg"
+	]
+
+	param.success(urls);
+
+	return;
+}
+
 function clickCard(element){
- 	cont++
+	cont++
 
 	let id = element.getAttribute("id")
 	let card = element.getAttribute("name")
 
-    element.classList.add("card-selected")
-	
-	if(cont==1){
+	element.classList.add("card-selected")
+
+	if(cont == 1){
 		divId = id
 		cardOne = card
 	}else{
 		cont = 0
 
-		if(card == cardOne){
+		if (card == cardOne) {
 			console.log("Cartas Iguais")
 			// Cartas iguais, ganha ponto, add css, desabilita
 			let disableCards = document.querySelectorAll("[name='"+card+"']")
@@ -41,8 +103,9 @@ function clickCard(element){
 			disableCards[0].classList.add("card-disable")
 			disableCards[1].onclick = null
 			disableCards[1].classList.add("card-disable")
+
 			clearClass(id, divId)
-		}else{
+		} else {
 			console.log("Vira o card para baixo")
 			clearClass(id, divId)
 		}
@@ -51,13 +114,11 @@ function clickCard(element){
 
 }
 
-function clearClass(id, div){
+function clearClass(divNow, divBefore){
 	setTimeout(()=>{
-		document.getElementById(id).classList.remove("card-selected")
-		document.getElementById(div).classList.remove("card-selected")
+		document.getElementById(divNow).classList.remove("card-selected")
+		document.getElementById(divBefore).classList.remove("card-selected")
 	}, 1500)    
 
 }
 
-//document.querySelectorAll("[name='3-myId']").innerHtml = "oiiiiii"
-//document.querySelectorAll("[name='3-myId']").classList.add("card-selected")
